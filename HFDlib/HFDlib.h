b562cfc4a3e47fc8cf760224b93b73e6948f4bcb
Sean Parker <skparker@gmail.com>
2014-06-08 11:42:22 -0700

    Initial Commit

---
+++ HFDlib/HFDlib.h
@@ -0,0 +1,13 @@
+//
+//  HFDlib.h
+//  HFDlib
+//
+//  Created by Sean Parker on 6/8/14.
+//  Copyright (c) 2014 LiteGear. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HFDlib : NSObject
+
+@end
---
+++ HFDlib/HFDlib.m
@@ -0,0 +1,13 @@
+//
+//  HFDlib.m
+//  HFDlib
+//
+//  Created by Sean Parker on 6/8/14.
+//  Copyright (c) 2014 LiteGear. All rights reserved.
+//
+
+#import "HFDlib.h"
+
+@implementation HFDlib
+
+@end
